aea0bc755eee92abbf4b606182cc78f76d73a966
mmirandar <mmirandar9@gmail.com>
2017-10-17 18:41:26 -0500

    Se Agregó la Entidad Proveedor

---
+++ Model.Entity/Proveedor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entity
+{
+    public class Proveedor
+    {
+        private long idProveedor;
+        private string nombre;
+        [Display(Name = "Apelido Materno")]
+        private string apmaterno;
+
+        private string appaterno;
+        private string ruc;
+        private string direccion;
+        private string telefono;
+        private int estado;
+
+        public int Estado
+        {
+            get
+            {
+                return estado;
+            }
+            set
+            {
+                estado = value;
+            }
+        }
+    }
+}
